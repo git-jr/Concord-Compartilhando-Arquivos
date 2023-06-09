31e6e9828588da3ea98fbede703b09d230bdb283
Junior <35709152+git-jr@users.noreply.github.com>
2023-06-09 16:28:22 -0300

    aula 1.4

--- app/src/main/java/com/alura/concord/navigation/MessageListScreen.kt
+++ app/src/main/java/com/alura/concord/navigation/MessageListScreen.kt
@@ -7,6 +7,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
@@ -19,11 +20,13 @@ import com.alura.concord.media.getNameByUri
 import com.alura.concord.media.imagePermission
 import com.alura.concord.media.persistUriPermission
 import com.alura.concord.media.verifyPermission
+import com.alura.concord.network.DownloadService.makeDownloadFileByURL
 import com.alura.concord.ui.chat.MessageListViewModel
 import com.alura.concord.ui.chat.MessageScreen
 import com.alura.concord.ui.components.ModalBottomSheetFile
 import com.alura.concord.ui.components.ModalBottomShareSheet
 import com.alura.concord.ui.components.ModalBottomSheetSticker
+import kotlinx.coroutines.launch
 
 internal const val messageChatRoute = "messages"
 internal const val messageChatIdArgument = "chatId"
@@ -52,7 +55,7 @@ fun NavGraphBuilder.messageListScreen(
                 }
             }
 
-
+            val scope = rememberCoroutineScope()
             MessageScreen(
                 state = uiState,
                 onSendMessage = {
@@ -75,6 +78,14 @@ fun NavGraphBuilder.messageListScreen(
                     onBack()
                 },
                 onContentDownload = { message ->
+
+                    scope.launch {
+                        makeDownloadFileByURL(
+                            "https://github.com/alura-cursos/jetpack-compose-armazenamento-arquivos-android/raw/arquivos/images/tiramisu.png",
+                            context
+                        )
+                    }
+
                     if (viewModelMessage.downloadInProgress()) {
                         viewModelMessage.startDownload(message)
                     } else {
--- app/src/main/java/com/alura/concord/network/DownloadService.kt
+++ app/src/main/java/com/alura/concord/network/DownloadService.kt
@@ -1,6 +1,8 @@
 package com.alura.concord.network
 
 import android.content.Context
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.File
@@ -8,19 +10,21 @@ import java.io.InputStream
 
 object DownloadService {
 
-    fun makeDownloadFileByURL(url: String, context: Context) {
+    suspend fun makeDownloadFileByURL(url: String, context: Context) {
         val client = OkHttpClient()
         val request = Request.Builder()
             .url(url)
             .build()
 
-        client.newCall(request).execute().let { response ->
-            response.body?.byteStream()?.let { fileData: InputStream ->
-                val path = context.getExternalFilesDir("temp")
-                val newFile = File(path, "Arquivo teste.png")
+        withContext(IO) {
+            client.newCall(request).execute().let { response ->
+                response.body?.byteStream()?.let { fileData: InputStream ->
+                    val path = context.getExternalFilesDir("temp")
+                    val newFile = File(path, "Arquivo teste.png")
 
-                newFile.outputStream().use { file ->
-                    fileData.copyTo(file)
+                    newFile.outputStream().use { file ->
+                        fileData.copyTo(file)
+                    }
                 }
             }
         }
