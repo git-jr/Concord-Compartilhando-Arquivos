887f85275a45c120060ef1897bbf49c5b04b9d6c
Junior <35709152+git-jr@users.noreply.github.com>
2023-06-10 16:48:53 -0300

    aula 2.1

--- app/src/main/java/com/alura/concord/network/DownloadService.kt
+++ app/src/main/java/com/alura/concord/network/DownloadService.kt
@@ -1,16 +1,17 @@
 package com.alura.concord.network
 
-import android.content.Context
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import java.io.File
 import java.io.InputStream
 
 object DownloadService {
 
-    suspend fun makeDownloadFileByURL(url: String, context: Context) {
+    suspend fun makeDownloadFileByURL(
+        url: String,
+        onFinishedDownload: (InputStream) -> Unit
+    ) {
         val client = OkHttpClient()
         val request = Request.Builder()
             .url(url)
@@ -19,12 +20,7 @@ object DownloadService {
         withContext(IO) {
             client.newCall(request).execute().let { response ->
                 response.body?.byteStream()?.let { fileData: InputStream ->
-                    val path = context.getExternalFilesDir("temp")
-                    val newFile = File(path, "Arquivo teste.png")
-
-                    newFile.outputStream().use { file ->
-                        fileData.copyTo(file)
-                    }
+                    onFinishedDownload(fileData)
                 }
             }
         }
